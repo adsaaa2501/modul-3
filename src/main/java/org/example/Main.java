571ee8e119c4f5a13af4587e1e9d3c2643a7b9dd
adsaaa2501 <adityasaputra2501@icloud.com>
2025-10-21 10:09:13 +0700

    ddd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
-public class DebugExample {
+public class Main {
     public static void main(String[] args) {
+
         int x = 5;
         int y = 10;
         int sum = x + y;
         System.out.println("Sum"  + sum);
+        System.out.println("nama Mahasiswa");
     }
 }
